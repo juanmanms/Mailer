3a2847187f1a476a0efd49902fdfdffc0dad8c5f
Juanmanms <juanmanms@gmail.com>
2023-12-15 22:59:23 +0100

    cambio a src

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ app.use(express.json());
 
 
 
-const subscribersRouter = require('./src/routes/subscribers');
+const subscribersRouter = require('./routes/subscribers');
 
 app.use('/subscribers', subscribersRouter);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}
+);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
