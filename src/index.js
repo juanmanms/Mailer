9114caf952fbcf2e6467ddb99b176b05e00fbc1a
Juanmanms <juanmanms@gmail.com>
2023-12-18 07:15:57 +0100

    install and add cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+//abrir cors para que se pueda acceder desde cualquier lugar
+const cors = require('cors');
+app.use(cors());
+
 
 
 const subscribersRouter = require('./routes/subscribers');
@@ -13,8 +17,8 @@ app.get('/', (req, res) => {
 );
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-  console.log('http://localhost:3000');
+  console.log('Server is running');
+
 });
 
   
